6422d2cc61489879969f75897f825752267960d7
Syed-Mubeen-11 <syedmubeen422@gmail.com>
2026-04-06 19:43:32 +0530

    Update celery_app.py

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -6,13 +6,13 @@ from celery.schedules import crontab
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-REDIS_URL = os.getenv("REDIS_URL")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 celery = Celery(
     "wealth_app",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
-    include=["app.tasks.price_refresh"]   # IMPORTANT
+    broker=REDIS_URL,
+    backend=REDIS_URL,
+    include=["app.tasks.price_refresh"]
 )
 
 celery.conf.timezone = "Asia/Kolkata"
